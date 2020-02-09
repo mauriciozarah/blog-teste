f3b6ff459ebc175604ab8e172d9be0ab39f2e2f2
Mauricio Zaha <mauriciozaha@gmail.com>
2020-02-09 20:22:01 -0300

    Inserido a description

--- application/views/control_panel/header/header.php
+++ application/views/control_panel/header/header.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+    <meta name="description" content="Portal de padarias delivery">
     <meta name="author" content="">
 
     <title>Blog v1.0</title>
