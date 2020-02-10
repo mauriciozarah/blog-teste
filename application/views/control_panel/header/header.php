4779295cce0caf7311acb4c8464135fdce6e6f27
Mauricio Zaha <mauriciozaha@gmail.com>
2020-02-09 22:06:43 -0300

    Titulo do Blog

--- application/views/control_panel/header/header.php
+++ application/views/control_panel/header/header.php
@@ -6,10 +6,10 @@
     <meta charset="iso-8859-1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Portal de padarias delivery">
+    <meta name="description" content="Portal de padarias Delivery">
     <meta name="author" content="">
 
-    <title>Blog v1.0</title>
+    <title>Blog v1.5</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="<?php echo base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css'); ?>" rel="stylesheet">
